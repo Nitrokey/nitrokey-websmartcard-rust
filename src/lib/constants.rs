4ebbcd4458e02322523cc2aaa7aec10bdf1217c5
Szczepan Zalega <szczepan@nitrokey.com>
2023-07-31 14:32:48 +0200

    Remove git_version dep

--- src/lib/constants.rs
+++ src/lib/constants.rs
@@ -1,6 +1,4 @@
-use git_version::git_version;
-
-pub const GIT_VERSION: &str = git_version!();
+pub const GIT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 // TODO rename to not confuse with FIDO2 RKs
 pub const RESIDENT_KEY_COUNT: usize = 50;
